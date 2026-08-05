e2684db3c45a8e8a018567529184f62b930f1b97
lifeaborted <yaz4ebalsya@yandex.ru>
2026-08-05 22:28:17 +0500

    refactor: extract IAudioEngine interface from BASS implementation

---
+++ src/core/audio/AudioEngine/IAudioEngine.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <functional>
+
+class IAudioEngine {
+public:
+    virtual ~IAudioEngine() = default;
+
+    // Основные методы плеера
+    virtual bool Init() = 0;
+    virtual bool PlayStream(const std::string& url, int durationSec, bool crossfade, const std::string& trackId = "") = 0;
+    virtual void Pause() = 0;
+    virtual void Resume() = 0;
+    virtual void SetVolume(float volume) = 0;
+    virtual void SetPositionSeconds(double pos) = 0;
+
+    // Геттеры
+    virtual float GetVolume() const = 0;
+    virtual bool IsPlaying() const = 0;
+    virtual double GetPositionSeconds() const = 0;
+    virtual double GetLengthSeconds() const = 0;
+    virtual std::vector<float> GetSpectrumData() const = 0;
+
+    std::function<void()> OnTrackNearEnd;
+    std::function<void()> OnTrackFinished;
+};
